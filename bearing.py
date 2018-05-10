5c438abc7f270938c1a376c438d12b9af7383504
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-05-10 08:59:46 -0500

    Update bearing.py

--- bearing.py
+++ bearing.py
@@ -51,7 +51,7 @@ def Stop():
 
 tup3 = (44.967861, -93.344609)
 (z1, z2) = tup3
-dist2 = math.hypot(z2 - z1, y2 - y1)   
+dist2 = math.hypot(x2 - x1, z2 - z1)   
 print "Distance 2 is: %r" %dist2
     
 new = time.time()
