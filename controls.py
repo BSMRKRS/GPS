a18be0f38fe79ac23e57cfd168db75115fecd5f2
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-05-17 09:13:28 -0500

    Add files via upload

---
+++ controls.py
@@ -0,0 +1,16 @@
+import RoboPiLib as RPL
+import time
+import math
+RPL.RoboPiInit("/dev/ttyAMA0",115200)
+
+
+f = open('bearing-output.txt', 'r')
+if f.read() == '+':
+    RPL.servoWrite(2, 1000)
+    RPL.servoWrite(3, 2000)
+if f.read() == '-':
+    RPL.servoWrite(2, 1000)
+    RPL.servoWrite(3, 2000)
+if f.read() == '0':
+    RPL.servoWrite(2, 1000)
+    RPL.servoWrite(3, 2000)
