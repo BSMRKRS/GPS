393720f7f68a2324e6696b490c9dd0e84f8e2787
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-05-17 09:21:47 -0500

    Update controls.py

--- controls.py
+++ controls.py
@@ -3,8 +3,8 @@ import time
 import math
 RPL.RoboPiInit("/dev/ttyAMA0",115200)
 
-
 f = open('bearing-output.txt', 'r')
+
 if f.read() == '+':
     RPL.servoWrite(2, 1000)
     RPL.servoWrite(3, 2000)
